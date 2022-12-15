c4fd65f1a05e990f0c88c15ba84dc551e3af6edb
Stephan Bogner <hello@stephanbogner.de>
2022-12-15 16:10:43 +0700

    Added media upload option

--- import.js
+++ import.js
@@ -1,5 +1,8 @@
 const debug = require('debug')('import');
 const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
 const moment = require('moment');
 const env = require('common-env/withLogger')(console);
 const config = env.getOrElseAll({
@@ -12,6 +15,7 @@ const config = env.getOrElseAll({
         $type: env.types.String,
       }
     },
+    postMedia: false,
     addDateFromTweet: false, // Adds '(15/9/2022) ' before the post (format depending on OS or below settings)
     changeDateLocaleTo: "", // Will default to OS settings
   },
@@ -21,6 +25,7 @@ const config = env.getOrElseAll({
     excludeRetweets: false,
     limitTweetsToSetYear: true,
     year: new Date().getFullYear(),
+    mediaPath: "",
     tweetjs: {
       filepath: {
         $type: env.types.String
@@ -75,15 +80,15 @@ function getTweets() {
 
   return tweets;
 }
-function importTweets(tweets) {
+async function importTweets(tweets) {
   const progress = require('progressbar').create().step('Importing tweets');
   const max = tweets.length;
 
   progress.setTotal(max);
 
-  next(); // Starts the process
+  await next(); // Starts the process
 
-  function next() {
+  async function next() {
     const tweet = tweets.pop();
     let current = 0;
     if (!tweet) {
@@ -91,24 +96,63 @@ function importTweets(tweets) {
       return;
     }
 
+    // 1. Prepare post text
     let postText = tweet.full_text
     if(config.mastodon.addDateFromTweet){
-      // Example from tweets.js "Mon Nov 02 23:45:58 +0000 2015"
-  
-      const tweetDate = moment(tweet.created_at, 'ddd MMM DD HH:mm:ss +-HHmm YYYY', 'en')
+      const tweetDate = moment(tweet.created_at, 'ddd MMM DD HH:mm:ss +-HHmm YYYY', 'en') // Date format example from tweets.js: "Mon Nov 02 23:45:58 +0000 2015"
       if(config.mastodon.changeDateLocaleTo !== ""){
         tweetDate.locale(config.mastodon.changeDateLocaleTo)
       }
       postText = `(${tweetDate.format('l')}) ${postText}`
     }
 
+    // 2. Load and upload media files as attachments
+    let mediaIds = []
+    if(config.mastodon.postMedia && tweet.extended_entities.media){
+      if(config.twitter.mediaPath == ""){
+        console.error('Please specify the path to the Twitter media folder')
+      }
+      const mediaToUpload = tweet.extended_entities.media
+
+      for(let i=0; i<mediaToUpload.length; i++){
+        const fileName = tweet.id + '-' + mediaToUpload[i].media_url_https.replace('https://pbs.twimg.com/media/', '')
+        const filePath = path.join(config.twitter.mediaPath, fileName)
+        if(!fs.existsSync(filePath)){
+          console.error("File doesn't exist at", filePath)
+          console.error("-> Quitting")
+          process.exit()
+        }
+        
+        const response = await uploadMediaAsAttachment(
+          {
+            apiToken: config.mastodon.api.key,
+            baseURL: config.mastodon.api.basePath
+          },
+          {
+            fileData: fs.readFileSync(filePath),
+            fileName: fileName
+          }
+        )
+        if(response.id){
+          mediaIds.push(response.id)
+        }else{
+          console.error("Uploading of", fileName, "failed")
+          console.error("-> Quitting")
+          process.exit()
+        }
+      }
+    }
+
+    // 3. Do the actual post
     createMastodonPost({
-      apiToken: config.mastodon.api.key,
-      baseURL: config.mastodon.api.basePath
-    },{
-      status: replaceTwitterUrls(postText,tweet.entities.urls),
-      language: tweet.lang
-    }).then((mastodonPost) => {
+        apiToken: config.mastodon.api.key,
+        baseURL: config.mastodon.api.basePath
+      },{
+        status: replaceTwitterUrls(postText,tweet.entities.urls),
+        language: tweet.lang,
+        mediaIds: mediaIds
+    })
+    .then((mastodonPost) => {
       debug('%s/%i Created post %s', current, max, mastodonPost.url);
       progress.addTick();
       next();
@@ -123,12 +167,37 @@ function replaceTwitterUrls(full_text, urls) {
   });
   return full_text;
 }
+
+function uploadMediaAsAttachment({
+  apiToken,
+  baseURL
+}, {
+  fileData,
+  fileName
+}) {
+  const formData = new FormData()
+  formData.append('file', fileData, fileName)
+  return axios({
+    url: '/api/v2/media',
+    baseURL: baseURL,
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + apiToken,
+      ...formData.getHeaders()
+    },
+    data: formData.getBuffer()
+  }).then(function(response) {
+    return response.data
+  });
+}
+
 function createMastodonPost({
   apiToken,
   baseURL
 }, {
   status,
-  language
+  language,
+  mediaIds
 }) {
   return axios({
     url: '/api/v1/statuses',
@@ -140,6 +209,7 @@ function createMastodonPost({
     data: {
       status,
       language,
+      media_ids: mediaIds,
       visibility: "public"
     }
   }).then(function(response) {
